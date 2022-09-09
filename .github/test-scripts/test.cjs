3292f39ce3582300f3688d505098386800904ba5
Dominic Griesel <dominic.griesel@nabucasa.com>
2022-09-09 11:14:46 +0200

    feat: convert to hybrid package

---
+++ .github/test-scripts/test.cjs
@@ -0,0 +1,4 @@
+const normalizeUrl = require("@esm2cjs/normalize-url").default;
+const assert = require("assert");
+
+assert(typeof normalizeUrl === "function");
---
+++ .github/test-scripts/test.mjs
@@ -0,0 +1,4 @@
+import normalizeUrl from "@esm2cjs/normalize-url";
+import assert from "assert";
+
+assert(typeof normalizeUrl === "function");
